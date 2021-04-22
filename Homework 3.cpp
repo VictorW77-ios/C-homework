63c7f432720d0b2509645f0178cd79685c968e8b
“VictorW77-ios” <victorwilliams1030@gmail.com>
2021-04-22 14:46:30 -0500

    compiled and ran program

---
+++ Homework 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main(void)
+{
+int ID, Hours
+cout << sizeof (ID);
+cout << sizeof Hours; 
+cout <<" enter ID \n"; 
+cin >> ID;
+cout <<" enter Hours \n";
+cin Hours;
+cout << " ID = " << Id <<endl;
+cout << " Hours = " << hour; 
+return 0;
+}
